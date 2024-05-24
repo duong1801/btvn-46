3d21c1ac17947e54200a427561de5027e8812c38
duong1801 <duongx1801@gmail.com>
2024-05-24 15:40:44 +0700

    update code

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,11 @@ import { useAuth0 } from "@auth0/auth0-react"
 function App() {
 	const { loginWithRedirect, user, isAuthenticated, isLoading } = useAuth0()
 
-	if (isLoading) {
-		return <div>Loading ...</div>
+	// if (isLoading) {
+	// 	return <div>Loading ...</div>
+	// }
+	{
+		console.log(user)
 	}
 	if (user) {
 		isAuthenticated && (
