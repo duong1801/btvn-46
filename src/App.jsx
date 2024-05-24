e5b10c9aa13b8f0fb5c9007142b5241ae95c1451
duong1801 <duongx1801@gmail.com>
2024-05-24 15:31:15 +0700

    update code

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,20 @@
 import { useAuth0 } from "@auth0/auth0-react"
 
 function App() {
-	const { loginWithRedirect } = useAuth0()
+	const { loginWithRedirect, user, isAuthenticated, isLoading } = useAuth0()
 
+	if (isLoading) {
+		return <div>Loading ...</div>
+	}
+	if (user) {
+		isAuthenticated && (
+			<div>
+				<img src={user.picture} alt={user.name} />
+				<h2>{user.name}</h2>
+				<p>{user.email}</p>
+			</div>
+		)
+	}
 	return <button onClick={() => loginWithRedirect()}>Log In</button>
 }
 
