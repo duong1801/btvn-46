e6a3f9b877b06a456573ffebef2979f46cd0508a
duong1801 <duongx1801@gmail.com>
2024-05-24 15:58:59 +0700

    update code

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,24 @@ import { useAuth0 } from "@auth0/auth0-react"
 function App() {
 	const { loginWithRedirect, user, isAuthenticated, isLoading } = useAuth0()
 
-	// if (isLoading) {
-	// 	return <div>Loading ...</div>
-	// }
-	{
-		console.log(user)
+	if (isLoading) {
+		return <div>Loading ...</div>
 	}
-	if (user) {
-		isAuthenticated && (
-			<div>
-				<img src={user.picture} alt={user.name} />
-				<h2>{user.name}</h2>
-				<p>{user.email}</p>
-			</div>
-		)
-	}
-	return <button onClick={() => loginWithRedirect()}>Log In</button>
+
+	return (
+		<>
+			{isAuthenticated || (
+				<button onClick={() => loginWithRedirect()}>Log In</button>
+			)}
+			{isAuthenticated && (
+				<div>
+					<img src={user?.picture} alt={user?.name} />
+					<h2>{user?.name}</h2>
+					<p>{user?.email}</p>
+				</div>
+			)}
+		</>
+	)
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -9,8 +9,9 @@ import { Auth0Provider } from "@auth0/auth0-react"
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<Auth0Provider
+			audience="https://dev-camw2msxs5buo2dw.us.auth0.com/api/v2"
 			domain="dev-camw2msxs5buo2dw.us.auth0.com"
-			clientId="a2ujv4c2q11GKnCkUkFohOYtLMu9c3dU"
+			clientId="3fmEm0WzVop0esdKtS0qgbRfvWHcNBw4"
 			redirectUri={window.location.origin}>
 			<App />
 		</Auth0Provider>
